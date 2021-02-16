35f4a5f541b3bda6790c363cc0d254c0f337501a
Thmsbonte <thms.bonte@gmail.com>
2021-02-17 00:13:42 +0100

    add checkout loading modal

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 import axios from "axios";
+import CheckoutModal from "./CheckoutModal";
+import { useHistory } from "react-router-dom";
 
 const CheckoutForm = ({ offer_id, user_id }) => {
   const [completed, setCompleted] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [loadingModal, setLoadingModal] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
+  const history = useHistory();
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
+    setLoadingModal(true);
 
     const cardElement = elements.getElement(CardElement);
     try {
@@ -27,50 +32,65 @@ const CheckoutForm = ({ offer_id, user_id }) => {
           }
         );
         response.data.status === "succeeded" && setCompleted(true);
+        setLoadingModal(false);
+        // Automatic redirection to home page
+        setTimeout(() => history.push("/"), 3000);
       } catch (error) {
         console.log(error.message);
         setErrorMessage(error.message);
+        setLoadingModal(false);
       }
     } catch (error) {
       console.log(error.message);
       setErrorMessage(error.message);
+      setLoadingModal(false);
     }
   };
 
   return !completed ? (
-    <div className="Checkout-form">
-      <div className="Checkout-order-summary">
-        <h5>Résumé de la commande</h5>
-        <div className="Checkout-order-summary-detail">
-          <p>Commande</p> <span>0 €</span>
+    <>
+      {loadingModal && <CheckoutModal />}
+      <div className="Checkout-form">
+        <div className="Checkout-order-summary">
+          <h5>Résumé de la commande</h5>
+          <div className="Checkout-order-summary-detail">
+            <p>Commande</p> <span>0 €</span>
+          </div>
+          <div className="Checkout-order-summary-detail">
+            <p>Frais protection acheteurs</p> <span>0 €</span>
+          </div>
+          <div className="Checkout-order-summary-detail">
+            <p>Frais de port</p> <span>0 €</span>
+          </div>
         </div>
-        <div className="Checkout-order-summary-detail">
-          <p>Frais protection acheteurs</p> <span>0 €</span>
+        <div className="Checkout-order-final">
+          <div className="Checkout-order-summary-total">
+            <p>Total</p> <span>0 €</span>
+          </div>
+          <p>
+            Il ne vous reste plus qu'un étape pour vous offrir null. Vous allez
+            payer 0 € (frais de protection et frais de port inclus).
+          </p>
         </div>
-        <div className="Checkout-order-summary-detail">
-          <p>Frais de port</p> <span>0 €</span>
-        </div>
+        <form onSubmit={handleOnSubmit}>
+          <div className="Card-element">
+            <CardElement />
+          </div>
+          <button type="submit">Pay</button>
+        </form>
       </div>
-      <div className="Checkout-order-final">
-        <div className="Checkout-order-summary-total">
-          <p>Total</p> <span>0 €</span>
-        </div>
-        <p>
-          Il ne vous reste plus qu'un étape pour vous offrir null. Vous allez
-          payer 0 € (frais de protection et frais de port inclus).
-        </p>
-      </div>
-      <form onSubmit={handleOnSubmit}>
-        <div className="Card-element">
-          <CardElement />
-        </div>
-        <button type="submit">Pay</button>
-      </form>
-    </div>
+    </>
   ) : errorMessage ? (
-    <p>Il y a eu une erreur :{errorMessage}. Merci de réessayer</p>
+    <div className="Checkout-error">
+      <p>Il y a eu une erreur :{errorMessage}. Merci de réessayer</p>
+    </div>
   ) : (
-    <p>Paiment réussi !</p>
+    <div className="Checkout-success">
+      <p>
+        <strong>Paiment réussi !</strong> Vous allez être automatiquement
+        redirigé vers la page d'accueil.
+      </p>
+    </div>
   );
 };
 
---
+++ src/components/CheckoutModal.js
@@ -0,0 +1,11 @@
+const CheckoutModal = () => {
+  return (
+    <div className="Modal">
+      <div className="Upload-modal">
+        <p>Paiement en cours. Merci de patienter.</p>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutModal;
