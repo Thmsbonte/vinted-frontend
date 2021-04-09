fba0e32e1a60fd42350fb5dc3c0e97b65ec29608
Thmsbonte <thms.bonte@gmail.com>
2021-04-09 10:12:22 +0200

    add signup regex

--- src/components/SignupModal.js
+++ src/components/SignupModal.js
@@ -28,7 +28,6 @@ const SignupModal = ({ setUser, modal, setModal }) => {
 
   // Function : save user profile picture object
   const handleFileChange = (event) => {
-    console.log(event.target.files[0].name);
     const newAvatarPreview = [...avatarPreview];
     newAvatarPreview.push(event.target.files[0].name);
     setAvatarPreview(newAvatarPreview);
@@ -40,48 +39,87 @@ const SignupModal = ({ setUser, modal, setModal }) => {
   // Function : handle form submit-> send new user sign-up data to the backend
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setErrorMessage(""); // Initialization of an error message state
-    setLoadingMessage(true); // Display of a loading message
+    setErrorMessage("");
+    setLoadingMessage(true); // Display of loading message
 
-    const formData = new FormData();
-    formData.append("username", credentials.username);
-    formData.append("email", credentials.email);
-    formData.append("phone", credentials.phone);
-    formData.append("password", credentials.password);
-    formData.append("newsletter", credentials.size);
-    formData.append("avatar", credentials.avatar);
+    // Username : alphabets, numbers, no special characters, min 6 and max 20 characters
+    const usernameRegex = new RegExp(/^[A-Za-z0-9]{6,20}$/);
+    // Standard email format
+    const emailRegex = new RegExp(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    );
+    // Password : 1 lowercase, 1 uppercase, 1 numeric, 1 special, 8 characters or longer
+    const passwordRegex = new RegExp(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/
+    );
+    // Standard french phone format
+    const phoneRegex = new RegExp(
+      /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/
+    );
 
-    try {
-      const response = await axios.post(
-        "https://lereacteur-vinted-backend.herokuapp.com/user/signup",
-        formData
-      );
-      setUser(
-        response.data.token,
-        response.data.account.username,
-        response.data._id
-      ); // Save user token and username information
-      setLoadingMessage(false);
+    if (usernameRegex.test(credentials.username)) {
+      if (emailRegex.test(credentials.email)) {
+        if (phoneRegex.test(credentials.phone)) {
+          if (passwordRegex.test(credentials.password)) {
+            // If all regex are validated, send the form
+            const formData = new FormData();
+            formData.append("username", credentials.username);
+            formData.append("email", credentials.email);
+            formData.append("phone", credentials.phone);
+            formData.append("password", credentials.password);
+            formData.append("newsletter", credentials.newsletter);
+            formData.append("avatar", credentials.avatar);
+            try {
+              const response = await axios.post(
+                "https://lereacteur-vinted-backend.herokuapp.com/user/signup",
+                formData
+              );
+              setUser(
+                response.data.token,
+                response.data.account.username,
+                response.data._id
+              ); // Save user token and username information
+              setLoadingMessage(false);
 
-      // If user comes from the "sell article" button, we redirect him to the publish page
-      if (modal.openingPage === "publish") {
-        const newModal = { ...modal };
-        newModal.signupModal = !modal.signupModal;
-        newModal.openingPage = "";
-        setModal(newModal);
-        history.push("/offer/publish");
+              // If user comes from the "sell article" button, we redirect him to the publish page
+              if (modal.openingPage === "publish") {
+                const newModal = { ...modal };
+                newModal.signupModal = !modal.signupModal;
+                newModal.openingPage = "";
+                setModal(newModal);
+                history.push("/offer/publish");
 
-        // Else redirection to the home page
+                // Else redirection to the home page
+              } else {
+                const newModal = { ...modal };
+                newModal.signupModal = !modal.signupModal;
+                setModal(newModal);
+                history.push("/");
+              }
+            } catch (error) {
+              setLoadingMessage(false);
+              console.log(error);
+              setErrorMessage(error.response.data.message); // Set an "error message" to display to the user
+            }
+          } else {
+            setLoadingMessage(false);
+            setErrorMessage(
+              "Le mot de passe doit contenir minimum 8 caractères dont 1 minuscule, 1 majuscule, un chiffre et un caractère spécial"
+            );
+          }
+        } else {
+          setLoadingMessage(false);
+          setErrorMessage("Format du téléphone invalide");
+        }
       } else {
-        const newModal = { ...modal };
-        newModal.signupModal = !modal.signupModal;
-        setModal(newModal);
-        history.push("/");
+        setLoadingMessage(false);
+        setErrorMessage("Format de l'email invalide");
       }
-    } catch (error) {
+    } else {
       setLoadingMessage(false);
-      console.log(error);
-      setErrorMessage(error.response.data.message); // Set an "error message" to display to the user
+      setErrorMessage(
+        "Votre nom d'utilisateur doit faire entre 6 et 20 caractères et ne doit pas contenir de caractère spécial"
+      );
     }
   };
 
